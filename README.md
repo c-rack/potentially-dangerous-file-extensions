bc976c4879e2245ea64566034bce696dec549528
Constantin Rack <constantin.rack@gmail.com>
2020-01-26 10:10:05 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # potentially-dangerous-file-extensions
-A list of potentially dangerous file extensions you might not want to download or as e-mail attachments
+Lists of potentially dangerous file extensions you might not want to download or as e-mail attachments
+
+## Available formats
+
+[Text file with one EXT per line](extenstions.txt)
+[Text file with one .EXT per line](extensions-with-dot.txt)
+[JSON array with EXT](extensions.json)
+[JSON array with .EXT](extensions-with-dot.json)
+
+## Contributing
+
+If you want to add an extension, edit `generate.sh` and then just run the script.
+Pull requests are welcome and I try to merge them quickly.
+
---
+++ generate.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+declare -a extensions=(
+  "ace"
+  "action"
+  "apk"
+  "app"
+  "bat"
+  "bin"
+  "cmd"
+  "com"
+  "command"
+  "cpl"
+  "csh"
+  "dll"
+  "dmg"
+  "docm"
+  "dotm"
+  "exe"
+  "gadget"
+  "inf1"
+  "ins"
+  "inx"
+  "ipa"
+  "isu"
+  "jar"
+  "job"
+  "js"
+  "jse"
+  "ksh"
+  "lnk"
+  "msc"
+  "msi"
+  "msp"
+  "mst"
+  "osx"
+  "out"
+  "paf"
+  "pif"
+  "prg"
+  "ps1"
+  "reg"
+  "rgs"
+  "run"
+  "scf"
+  "scr"
+  "sct"
+  "sh"
+  "shb"
+  "shs"
+  "sys"
+  "u3p"
+  "vb"
+  "vbe"
+  "vbs"
+  "vbscript"
+  "workflow"
+  "ws"
+  "wsf"
+  "wsh"
+  "xlsm"
+)
+
+rm -f extensions.txt extensions-wth-dot.txt extensions.json extensions-with-dot.json
+
+for ext in "${extensions[@]}"
+do
+  echo "$ext" >>extensions.txt
+  echo ".$ext" >>extensions-with-dot.txt
+done
+
+cat extensions.txt | jq  --raw-input .  | jq --slurp . >extensions.json
+cat extensions-with-dot.txt | jq  --raw-input .  | jq --slurp . >extensions-with-dot.json
+
